62202390ee8f3ae9f41254dafbc0f160cd675955
Christy Moormann <christy@christymoormann.com>
2019-01-05 15:29:37 -0800

    Added rollup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import Post from './components/Post';
+// Import vue component
+import Post from './Post';
 
-export default {
-    install(Vue) {
-        Vue.component('post', Post);
-    }
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component('Post', Post);
+}
+
+// Create module definition for Vue.use()
+const plugin = {
+    install,
 };
 
-export { Post };
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+    GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+// To allow use as module (npm/webpack/etc.) export component
+export default Post;
